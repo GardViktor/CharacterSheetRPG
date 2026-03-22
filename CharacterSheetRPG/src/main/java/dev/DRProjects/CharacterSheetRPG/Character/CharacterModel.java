3512c6d875a5e82a5ed3e0dc7f513da3e2ceb0a9
Edgard <edgardvictorlol@gmail.com>
2026-03-22 20:55:38 -0300

    feat: add CharacterModel with D&D 5e attributes

--- CharacterSheetRPG/src/main/java/dev/DRProjects/CharacterSheetRPG/Character/CharacterModel.java
+++ CharacterSheetRPG/src/main/java/dev/DRProjects/CharacterSheetRPG/Character/CharacterModel.java
@@ -1,4 +1,28 @@
 package dev.DRProjects.CharacterSheetRPG.Character;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_character")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CharacterModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false) // Character name cannot be empty
+    private String characterName;
+    private String characterClass;
+    private String background;
+    private String race;
+    private String alignment;
+    private String gender;
+    private int age;
+    private int level;
+    private int experiencePoints;
 }
