7a75b43b81e011d7cb33332334c21be8f7d8c356
Edgard <edgardvictorlol@gmail.com>
2026-03-22 20:44:03 -0300

    chore: add CharacterController smoke test endpoint

--- CharacterSheetRPG/src/main/java/dev/DRProjects/CharacterSheetRPG/Character/CharacterController.java
+++ CharacterSheetRPG/src/main/java/dev/DRProjects/CharacterSheetRPG/Character/CharacterController.java
@@ -1,4 +1,15 @@
 package dev.DRProjects.CharacterSheetRPG.Character;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class CharacterController {
+
+    @GetMapping("/character")
+        public String character(){
+        return "method character() PASSED";
+    }
 }
